41ed325a2c907528374b12de5d6def39dcb50f2d
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-07-24 14:39:37 -0500

    Add main file for provider

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/plugin"
+	"github.com/terraform-providers/terraform-provider-cloudscale/cloudscale"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: cloudscale.Provider,
+	})
+}
