90340bed4164e36ba54ca4322e6ca46631857ca4
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-07-24 14:41:22 -0500

    Add cloudscale provider

---
+++ cloudscale/config.go
@@ -0,0 +1,20 @@
+package cloudscale
+
+import (
+	"github.com/cloudscale-ch/cloudscale"
+	"golang.org/x/oauth2"
+)
+
+type Config struct {
+	Token string
+}
+
+func (c *Config) Client() (*cloudscale.Client, error) {
+
+	tc := oauth2.NewClient(oauth2.NoContext, oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: c.Token},
+	))
+	client := cloudscale.NewClient(tc)
+
+	return client, nil
+}
---
+++ cloudscale/provider.go
@@ -0,0 +1,29 @@
+package cloudscale
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"token": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CLOUDSCALE_TOKEN", nil),
+				Description: "The token for API operations.",
+			},
+		},
+
+		ResourcesMap:  map[string]*schema.Resource{},
+		ConfigureFunc: providerConfigureClient,
+	}
+}
+
+func providerConfigureClient(d *schema.ResourceData) (interface{}, error) {
+	config := Config{
+		Token: d.Get("token").(string),
+	}
+	return config.Client()
+}
