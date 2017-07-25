9365ef2d60ca40192b32cc3f1e4f05ab8cdd4728
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-07-25 02:43:38 -0500

    Add Resource Server functionality

---
+++ cloudscale/provider_cloudscale_server.go
@@ -0,0 +1,380 @@
+package cloudscale
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/cloudscale-ch/cloudscale"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceCloudScaleServer() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceServerCreate,
+		Read:   resourceServerRead,
+		Update: resourceServerUpdate,
+		Delete: resourceServerDelete,
+
+		Schema: map[string]*schema.Schema{
+			"href": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"flavor": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"image": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"volume_size_gb": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+				ForceNew: true,
+			},
+			"bulk_volume_size_gb": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+			},
+			"volumes": {
+				Type: schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"device_path": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"size_gb": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+					},
+				},
+				Computed: true,
+			},
+			"ssh_keys": {
+				Type:     schema.TypeList,
+				Required: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				ForceNew: true,
+			},
+			"use_public_network": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				ForceNew: true,
+			},
+			"use_private_network": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				ForceNew: true,
+			},
+			"use_ipv6": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				ForceNew: true,
+			},
+			"anti_affinity_with": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				ForceNew: true,
+			},
+			"user_data": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"ipv4_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ipv6_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ipv4_private_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ipv6_private_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ssh_fingerprints": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+			"ssh_host_keys": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"state": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+	}
+}
+func resourceServerCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudscale.Client)
+
+	opts := &cloudscale.ServerRequest{
+		Name:   d.Get("name").(string),
+		Flavor: d.Get("flavor").(string),
+		Image:  d.Get("image").(string),
+	}
+
+	sshKeys := d.Get("ssh_keys.#").(int)
+	if sshKeys > 0 {
+		opts.SSHKeys = make([]string, 0, sshKeys)
+		for i := 0; i < sshKeys; i++ {
+			key := fmt.Sprintf("ssh_keys.%d", i)
+			sshkey := d.Get(key).(string)
+			opts.SSHKeys = append(opts.SSHKeys, sshkey)
+
+		}
+	}
+
+	if attr, ok := d.GetOk("volume_size_gb"); ok {
+		opts.VolumeSizeGB = attr.(int)
+	}
+
+	if attr, ok := d.GetOk("bulk_volume_size_gb"); ok {
+		opts.BulkVolumeSizeGB = attr.(int)
+	}
+
+	if attr, ok := d.GetOk("use_public_network"); ok {
+		opts.UsePublicNetwork = attr.(bool)
+	}
+
+	if attr, ok := d.GetOk("use_private_network	"); ok {
+		opts.UsePrivateNetwork = attr.(bool)
+	}
+
+	if attr, ok := d.GetOk("use_ipv6"); ok {
+		opts.UseIPV6 = attr.(bool)
+	}
+
+	antiAffinityUUIDs := d.Get("anti_affinity_with.#").(int)
+	if antiAffinityUUIDs > 0 {
+		opts.AntiAffinityWith = make([]string, 0, antiAffinityUUIDs)
+
+		for i := 0; i < antiAffinityUUIDs; i++ {
+			key := fmt.Sprintf("anti_affinity_with.%d", i)
+			antiAffinityUUID := d.Get(key).(string)
+			opts.AntiAffinityWith = append(opts.AntiAffinityWith, antiAffinityUUID)
+
+		}
+	}
+
+	if attr, ok := d.GetOk("user_data"); ok {
+		opts.UserData = attr.(string)
+	}
+
+	log.Printf("[DEBUG] Server create configuration: %#v", opts)
+
+	server, err := client.Servers.Create(context.Background(), opts)
+	if err != nil {
+		return fmt.Errorf("Error creating server: %s", err)
+	}
+
+	d.SetId(server.UUID)
+
+	log.Printf("[INFO] Server ID %s", d.Id())
+
+	_, err = waitForServerStatus(d, meta, []string{"changing"}, "status", "running")
+	if err != nil {
+		return fmt.Errorf("Error waiting for server (%s) to become ready %s", d.Id(), err)
+	}
+
+	return resourceServerRead(d, meta)
+}
+
+func resourceServerRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudscale.Client)
+
+	id := d.Id()
+
+	server, err := client.Servers.Get(context.Background(), id)
+	if err != nil {
+		if err.Error() == "detail: Not Found." {
+			log.Printf("[WARN] Cloudscale Server (%s) not found", d.Id())
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("Error retrieving server: %s", err)
+	}
+
+	d.Set("href", server.HREF)
+	d.Set("name", server.Name)
+	d.Set("flavor", server.Flavor.Slug)
+	d.Set("image", server.Image.Slug)
+
+	if volumes := len(server.Volumes); volumes > 0 {
+		volumesMaps := make([]map[string]interface{}, 0, volumes)
+		for _, volume := range server.Volumes {
+			v := make(map[string]interface{})
+			v["type"] = volume.Type
+			v["device_path"] = volume.DevicePath
+			v["size_gb"] = volume.SizeGB
+			volumesMaps = append(volumesMaps, v)
+		}
+		d.Set("volumes", volumesMaps)
+	}
+
+	d.Set("status", server.Status)
+
+	if publicIPv4 := findIPv4AddrByType(server, "public"); publicIPv4 != "" {
+		d.Set("ipv4_address", publicIPv4)
+	}
+	if publicIPv6 := findIPv6AddrByType(server, "public"); publicIPv6 != "" {
+		d.Set("ipv6_address", publicIPv6)
+	}
+	if privateIPv4 := findIPv4AddrByType(server, "private"); privateIPv4 != "" {
+		d.Set("ipv4_private_address", privateIPv4)
+	}
+	if privateIPv6 := findIPv4AddrByType(server, "private"); privateIPv6 != "" {
+		d.Set("ipv6_private_address", privateIPv6)
+	}
+
+	d.Set("ssh_fingerprints", server.SSHFingerprints)
+
+	d.Set("ssh_host_keys", server.SSHHostKeys)
+
+	if antiAffinities := len(server.AntiAfinityWith); antiAffinities > 0 {
+		antiAfs := make([]string, 0, antiAffinities)
+		for _, antiAf := range server.AntiAfinityWith {
+			antiAfs = append(antiAfs, antiAf.UUID)
+		}
+		d.Set("anti_affinity_with", antiAfs)
+	}
+
+	d.SetConnInfo(map[string]string{
+		"type": "ssh",
+		"host": findIPv4AddrByType(server, "public"),
+	})
+
+	return nil
+}
+
+func resourceServerUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudscale.Client)
+	id := d.Id()
+
+	if d.HasChange("state") {
+		state := d.Get("state").(string)
+		err := client.Servers.Update(context.Background(), id, state)
+		if err != nil {
+			return fmt.Errorf("Error updating the Server (%s) state (%s) ", id, err)
+		}
+
+	}
+
+	return resourceServerRead(d, meta)
+}
+
+func resourceServerDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudscale.Client)
+	id := d.Id()
+
+	log.Printf("[INFO] Deleting Server: %s", d.Id())
+	err := client.Servers.Delete(context.Background(), id)
+
+	if err != nil && strings.Contains(err.Error(), "Not found") {
+		return nil
+	}
+
+	if err != nil {
+		return fmt.Errorf("Error deleting droplet: %s", err)
+	}
+
+	return nil
+}
+
+func waitForServerStatus(d *schema.ResourceData, meta interface{}, pending []string, attribute, target string) (interface{}, error) {
+	log.Printf(
+		"[INFO] Waiting for server (%s) to have %s of %s",
+		d.Id(), attribute, target)
+
+	stateConf := &resource.StateChangeConf{
+		Pending:    pending,
+		Target:     []string{target},
+		Refresh:    newServerRefreshFunc(d, attribute, meta),
+		Timeout:    60 * time.Minute,
+		Delay:      10 * time.Second,
+		MinTimeout: 3 * time.Second,
+	}
+
+	return stateConf.WaitForState()
+}
+
+func newServerRefreshFunc(d *schema.ResourceData, attribute string, meta interface{}) resource.StateRefreshFunc {
+	client := meta.(*cloudscale.Client)
+	return func() (interface{}, string, error) {
+		id := d.Id()
+
+		err := resourceServerRead(d, meta)
+		if err != nil {
+			return nil, "", err
+		}
+
+		if attr, ok := d.GetOk(attribute); ok {
+			server, err := client.Servers.Get(context.Background(), id)
+			if err != nil {
+				return nil, "", fmt.Errorf("Error retrieving server %s", err)
+			}
+
+			return server, attr.(string), nil
+		}
+		return nil, "", nil
+	}
+}
+
+func findIPv6AddrByType(s *cloudscale.Server, addrType string) string {
+	for _, interf := range s.Interfaces {
+		if interf.Type == addrType {
+			for _, addr := range interf.Adresses {
+				if addr.Version == 6 {
+					return addr.Address
+				}
+			}
+		}
+	}
+	return ""
+}
+
+func findIPv4AddrByType(s *cloudscale.Server, addrType string) string {
+	for _, interf := range s.Interfaces {
+		if interf.Type == addrType {
+			for _, addr := range interf.Adresses {
+				if addr.Version == 4 {
+					return addr.Address
+				}
+			}
+		}
+	}
+	return ""
+}
---
+++ cloudscale/provider_cloudscale_server_test.go
@@ -0,0 +1,162 @@
+package cloudscale
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"testing"
+
+	"github.com/cloudscale-ch/cloudscale"
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func init() {
+	resource.AddTestSweepers("cloudscale_server", &resource.Sweeper{
+		Name: "cloudscale_server",
+		F:    testSweepServers,
+	})
+
+}
+
+func testSweepServers(region string) error {
+	meta, err := sharedConfigForRegion(region)
+	if err != nil {
+		return err
+	}
+
+	client := meta.(*cloudscale.Client)
+
+	servers, err := client.Servers.List(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, s := range servers {
+		if strings.HasPrefix(s.Name, "terraform-") {
+			log.Printf("Destroying Server %s", s.Name)
+
+			if err := client.Servers.Delete(context.Background(), s.UUID); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func TestAccCloudscale_Basic(t *testing.T) {
+	var server cloudscale.Server
+
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudscaleServerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckCloudscaleServerConfig_basic(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCloudscaleServerExists("cloudscale_server.basic", &server),
+					testAccCheckCloudscaleServerAttributes(&server),
+					resource.TestCheckResourceAttr(
+						"cloudscale_server.basic", "name", fmt.Sprintf("terraform-%d", rInt)),
+					resource.TestCheckResourceAttr(
+						"cloudscale_server.basic", "flavor", "flex-2"),
+					resource.TestCheckResourceAttr(
+						"cloudscale_server.basic", "image", "debian-8"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckCloudscaleServerDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*cloudscale.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "cloudscale_server" {
+			continue
+		}
+
+		id := rs.Primary.ID
+
+		// Try to find the Droplet
+		_, err := client.Servers.Get(context.Background(), id)
+
+		// Wait
+
+		if err != nil && !strings.Contains(err.Error(), "Not found") {
+			return fmt.Errorf(
+				"Error waiting for server (%s) to be destroyed: %s",
+				rs.Primary.ID, err)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckCloudscaleServerExists(n string, server *cloudscale.Server) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Server ID is set")
+		}
+
+		client := testAccProvider.Meta().(*cloudscale.Client)
+
+		id := rs.Primary.ID
+
+		// Try to find the Droplet
+		retrieveServer, err := client.Servers.Get(context.Background(), id)
+
+		if err != nil {
+			return err
+		}
+
+		if retrieveServer.UUID != rs.Primary.ID {
+			return fmt.Errorf("Server not found")
+		}
+
+		*server = *retrieveServer
+
+		return nil
+	}
+}
+
+func testAccCheckCloudscaleServerAttributes(server *cloudscale.Server) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		if server.Image.Slug != "debian-8" {
+			return fmt.Errorf("Bad image_slug: %s", server.Image.Slug)
+		}
+
+		if server.Flavor.Slug != "flex-2" {
+			return fmt.Errorf("Bad flavor_slug: %s", server.Image.Slug)
+		}
+
+		if server.Volumes[0].SizeGB != 10 {
+			return fmt.Errorf("Bad volumes_size_gb: %d", server.Volumes[0].SizeGB)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckCloudscaleServerConfig_basic(rInt int) string {
+	return fmt.Sprintf(`
+resource "cloudscale_server" "basic" {
+  name      			= "terraform-%d"
+  flavor    			= "flex-2"
+  image     			= "debian-8"
+  volume_size_gb	= 10
+  ssh_keys = ["ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFEepRNW5hDct4AdJ8oYsb4lNP5E9XY5fnz3ZvgNCEv7m48+bhUjJXUPuamWix3zigp2lgJHC6SChI/okJ41GUY="]
+}`, rInt)
+}
