3a79eadf7d0d1886f9053e7dd087a90dadce0ec2
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-07-25 02:43:13 -0500

    Add sweeper function

---
+++ cloudscale/sweeper_test.go
@@ -0,0 +1,31 @@
+package cloudscale
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestMain(m *testing.M) {
+	resource.TestMain(m)
+}
+
+func sharedConfigForRegion(region string) (interface{}, error) {
+	if os.Getenv("CLOUDSCALE_TOKEN") == "" {
+		return nil, fmt.Errorf("empty CLOUDSCALE_TOKEN")
+	}
+
+	config := Config{
+		Token: os.Getenv("CLOUDSCALE_TOKEN"),
+	}
+
+	// configures a default client for the region, using the above env vars
+	client, err := config.Client()
+	if err != nil {
+		return nil, fmt.Errorf("error getting CloudScale client")
+	}
+
+	return client, nil
+}
