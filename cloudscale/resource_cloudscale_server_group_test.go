d98b07d73b4a97f78c63a3ec641623a1ddce3651
Dave Halter <davidhalter88@gmail.com>
2019-06-12 18:26:42 +0200

    Add integration tests for server groups

---
+++ cloudscale/resource_cloudscale_server_group_test.go
@@ -0,0 +1,81 @@
+package cloudscale
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"testing"
+
+	"github.com/cloudscale-ch/cloudscale-go-sdk"
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func init() {
+	resource.AddTestSweepers("cloudscale_server_group", &resource.Sweeper{
+		Name: "cloudscale_server_group",
+		F:    testSweepServerGroups,
+	})
+}
+
+func testSweepServerGroups(region string) error {
+	meta, err := sharedConfigForRegion(region)
+	if err != nil {
+		return err
+	}
+
+	client := meta.(*cloudscale.Client)
+
+	serverGroups, err := client.ServerGroups.List(context.Background())
+	if err != nil {
+		return err
+	}
+
+	foundError := error(nil)
+	for _, s := range serverGroups {
+		if strings.HasPrefix(s.Name, "terraform-") {
+			log.Printf("Destroying server group %s", s.Name)
+
+			if err := client.ServerGroups.Delete(context.Background(), s.UUID); err != nil {
+				foundError = err
+			}
+		}
+	}
+	return foundError
+}
+
+func TestAccCloudscaleServerGroup_Basic(t *testing.T) {
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudscaleServerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckCloudscaleServerGroupConfig(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"cloudscale_server.some_server", "server_groups.0.name", fmt.Sprintf("terraform-%d-group", rInt)),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckCloudscaleServerGroupConfig(rInt int) string {
+	return fmt.Sprintf(`
+resource "cloudscale_server_group" "ayyy" {
+  name = "terraform-%d-group"
+  type = "anti-affinity"
+}
+
+resource "cloudscale_server" "some_server" {
+  name                      = "terraform-%d-foobar"
+  server_group_ids          = ["${cloudscale_server_group.ayyy.id}"]
+  flavor_slug               = "flex-4"
+  image_slug                = "%s"
+  ssh_keys                  = ["ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFEepRNW5hDct4AdJ8oYsb4lNP5E9XY5fnz3ZvgNCEv7m48+bhUjJXUPuamWix3zigp2lgJHC6SChI/okJ41GUY=", "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFEepRNW5hDct4AdJ8oYsb4lNP5E9XY5fnz3ZvgNCEv7m48+bhUjJXUPuamWix3zigp2lgJHC6SChI/okJ41GUY="]
+}`, rInt, rInt, DefaultImageSlug)
+}
