4df97467754e3e7c2975cea723cfd2c04ad9e811
Dave Halter <davidhalter88@gmail.com>
2021-10-25 12:59:44 +0200

    Actually add the floating IP datasource

---
+++ cloudscale/datasource_cloudscale_floating_ip.go
@@ -0,0 +1,30 @@
+package cloudscale
+
+import (
+	"context"
+
+	"github.com/cloudscale-ch/cloudscale-go-sdk"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceCloudscaleFloatingIP() *schema.Resource {
+	recordSchema := getFloatingIPSchema(true)
+
+	return &schema.Resource{
+		ReadContext: dataSourceResourceRead("floating_ip", recordSchema, floatingIPsRead),
+		Schema:      recordSchema,
+	}
+}
+
+func floatingIPsRead(meta interface{}) ([]ResourceDataRaw, error) {
+	client := meta.(*cloudscale.Client)
+	floatingIPList, err := client.FloatingIPs.List(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	var rawItems []ResourceDataRaw
+	for _, floating_ip := range floatingIPList {
+		rawItems = append(rawItems, gatherFloatingIPResourceData(&floating_ip))
+	}
+	return rawItems, nil
+}
