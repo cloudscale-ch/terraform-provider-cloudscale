ca7f0e829ab97440a53213d69525769eb9f0cb66
Alain Kaeslin <alain@cloudscale.ch>
2022-12-20 17:07:38 +0100

    Remove unused code.

--- cloudscale/resource_cloudscale_custom_image.go
+++ cloudscale/resource_cloudscale_custom_image.go
@@ -23,9 +23,6 @@ func resourceCloudscaleCustomImage() *schema.Resource {
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(20 * time.Minute),
 		},
-		/*Importer: &schema.ResourceImporter{
-			StateContext: resourceCloudscaleCustomImageImport,
-		},*/
 	}
 }
 
@@ -286,9 +283,4 @@ func newCustomImageImportRefreshFunc(uuid string, d *schema.ResourceData, attrib
 
 		return customImageImport, customImageImport.Status, nil
 	}
-}
-
-func resourceCloudscaleCustomImageImport(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
-	d.Set("import_uuid", d.Id())
-	return schema.ImportStatePassthroughContext(ctx, d, meta)
 }
