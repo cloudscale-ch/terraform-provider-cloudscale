49f89eb4aa8f1d86c6199177baaf18091c8befbc
Alain Kaeslin <alain@cloudscale.ch>
2022-12-30 14:42:17 +0100

    Add Update tests.

--- cloudscale/resource_cloudscale_load_balancer_health_monitor_test.go
+++ cloudscale/resource_cloudscale_load_balancer_health_monitor_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/cloudscale-ch/cloudscale-go-sdk/v2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"testing"
 )
 
@@ -25,7 +26,7 @@ func TestAccCloudscaleLoadBalancerHealthMonitor_Basic(t *testing.T) {
 			{
 				Config: testAccCloudscaleLoadBalancerConfig_basic(rInt) +
 					testAccCloudscaleLoadBalancerPoolConfig_basic(rInt) +
-					testAccCloudscaleLoadBalancerHealthMonitorConfig_basic(),
+					testAccCloudscaleLoadBalancerHealthMonitorConfig_basic(10),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckCloudscaleLoadBalancerExists("cloudscale_load_balancer.lb-acc-test", &loadBalancer),
 					testAccCheckCloudscaleLoadBalancerPoolExists("cloudscale_load_balancer_pool.lb-pool-acc-test", &loadBalancerPool),
@@ -52,15 +53,138 @@ func TestAccCloudscaleLoadBalancerHealthMonitor_Basic(t *testing.T) {
 	})
 }
 
-func testAccCloudscaleLoadBalancerHealthMonitorConfig_basic() string {
+func TestAccCloudscaleLoadBalancerHealthMonitor_UpdateDelay(t *testing.T) {
+	var afterCreate, afterUpdate cloudscale.LoadBalancerHealthMonitor
+
+	rInt1 := acctest.RandInt()
+	rInt2 := acctest.RandInt()
+
+	resourceName := "cloudscale_load_balancer_health_monitor.lb-health_monitor-acc-test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudscaleLoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudscaleLoadBalancerConfig_basic(rInt1) +
+					testAccCloudscaleLoadBalancerPoolConfig_basic(rInt1) +
+					testAccCloudscaleLoadBalancerHealthMonitorConfig_basic(rInt1),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCloudscaleLoadBalancerHealthMonitorExists(resourceName, &afterCreate),
+					resource.TestCheckResourceAttr(
+						resourceName, "delay", fmt.Sprintf("%v", rInt1)),
+				),
+			},
+			{
+				Config: testAccCloudscaleLoadBalancerConfig_basic(rInt1) +
+					testAccCloudscaleLoadBalancerPoolConfig_basic(rInt1) +
+					testAccCloudscaleLoadBalancerHealthMonitorConfig_basic(rInt2),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCloudscaleLoadBalancerHealthMonitorExists(resourceName, &afterUpdate),
+					resource.TestCheckResourceAttr(
+						resourceName, "delay", fmt.Sprintf("%v", rInt1)),
+					testAccCheckLoadBalancerHealthMonitorIsSame(t, &afterCreate, &afterUpdate, true),
+				),
+			},
+		},
+	})
+}
+
+func TestAccCloudscaleLoadBalancerHealthMonitor_UpdatePool(t *testing.T) {
+	var pool1, pool2 cloudscale.LoadBalancerPool
+	var afterCreate, afterUpdate cloudscale.LoadBalancerHealthMonitor
+
+	resourceName := "cloudscale_load_balancer_health_monitor.lb-health_monitor-acc-test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudscaleLoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudscaleLoadBalancerHealthMonitorConfig_multiple(15, 0),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCloudscaleLoadBalancerPoolExists("cloudscale_load_balancer_pool.lb-pool-acc-test.0", &pool1),
+					testAccCheckCloudscaleLoadBalancerPoolExists("cloudscale_load_balancer_pool.lb-pool-acc-test.1", &pool2),
+					testAccCheckCloudscaleLoadBalancerHealthMonitorExists(resourceName, &afterCreate),
+					resource.TestCheckResourceAttrPtr(
+						resourceName, "pool_uuid", &pool1.UUID),
+					resource.TestCheckResourceAttrPair(
+						resourceName, "pool_uuid",
+						"cloudscale_load_balancer_pool.lb-pool-acc-test.0", "id"),
+				),
+			},
+			{
+				Config: testAccCloudscaleLoadBalancerHealthMonitorConfig_multiple(15, 1),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCloudscaleLoadBalancerHealthMonitorExists(resourceName, &afterUpdate),
+					testAccCheckLoadBalancerHealthMonitorIsSame(t, &afterCreate, &afterUpdate, false),
+					resource.TestCheckResourceAttrPtr(
+						resourceName, "pool_uuid", &pool2.UUID),
+					resource.TestCheckResourceAttrPair(
+						resourceName, "pool_uuid",
+						"cloudscale_load_balancer_pool.lb-pool-acc-test.1", "id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckLoadBalancerHealthMonitorIsSame(t *testing.T,
+	before *cloudscale.LoadBalancerHealthMonitor, after *cloudscale.LoadBalancerHealthMonitor,
+	expectSame bool) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if adr := before; adr == after {
+			t.Fatalf("Passed the same instance twice, address is equal=%v",
+				adr)
+		}
+		isSame := before.UUID == after.UUID
+		if isSame != expectSame {
+			t.Fatalf("Unexpected LoadBalancerPoolMember IDs got=%s, expected=%s, isSame=%t",
+				after.UUID, before.UUID, isSame)
+		}
+		return nil
+	}
+}
+
+func testAccCloudscaleLoadBalancerHealthMonitorConfig_multiple(rInt int, poolIndex int) string {
 	return fmt.Sprintf(`
+resource "cloudscale_load_balancer" "lb-acc-test" {
+  name        = "terraform-%[1]d-lb"
+  flavor_slug = "lb-flex-4-2"
+  zone_slug   = "rma1"
+}
+
+resource "cloudscale_load_balancer_pool" "lb-pool-acc-test" {
+  count              = 2
+  name               = "terraform-%[1]d-lb-pool-${count.index}"
+  load_balancer_uuid = cloudscale_load_balancer.lb-acc-test.id
+  algorithm          = "round_robin"
+  protocol           = "tcp"
+}
+
 resource "cloudscale_load_balancer_health_monitor" "lb-health_monitor-acc-test" {
-  pool_uuid        = cloudscale_load_balancer_pool.lb-pool-acc-test.id
-  delay            = 10
+  pool_uuid        = cloudscale_load_balancer_pool.lb-pool-acc-test[%[2]d].id
+  delay            = %[1]d
   max_retries      = 3
   max_retries_down = 3
   timeout          = 5
   type             = "tcp"
 }
-`)
+`, rInt, poolIndex)
+
+}
+
+func testAccCloudscaleLoadBalancerHealthMonitorConfig_basic(rInt int) string {
+	return fmt.Sprintf(`
+resource "cloudscale_load_balancer_health_monitor" "lb-health_monitor-acc-test" {
+  pool_uuid        = cloudscale_load_balancer_pool.lb-pool-acc-test.id
+  delay            = %v
+  max_retries      = 3
+  max_retries_down = 3
+  timeout          = 5
+  type             = "tcp"
+}
+`, rInt)
 }
