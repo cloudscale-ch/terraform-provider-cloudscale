e4d82836b527273b376338e6431c7b7987e76097
Alain Kaeslin <alain@cloudscale.ch>
2026-04-10 17:27:49 +0200

    chore: upgrade to cloudscale-go-sdk/v9

--- cloudscale/utils_test.go
+++ cloudscale/utils_test.go
@@ -2,6 +2,9 @@ package cloudscale
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/cloudscale-ch/cloudscale-go-sdk/v9"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
